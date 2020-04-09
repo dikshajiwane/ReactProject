f6ae662ba625f91448fa623bc6e67617757334fb
Jiwane <diksha1994j@gmail.com>
2020-04-09 23:54:33 +0530

    Updates

---
+++ precache-manifest.0feda258a77ec2b88b70babc7897861c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ac1ca43f1bbeac974f30ecf687e269",
+    "url": "/dikshajiwane.github.io/contactmanager/index.html"
+  },
+  {
+    "revision": "d36f5b298238bed16ebc",
+    "url": "/dikshajiwane.github.io/contactmanager/static/css/2.0e3bee50.chunk.css"
+  },
+  {
+    "revision": "40d6f2705994ad452be3",
+    "url": "/dikshajiwane.github.io/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d36f5b298238bed16ebc",
+    "url": "/dikshajiwane.github.io/contactmanager/static/js/2.043f57b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dikshajiwane.github.io/contactmanager/static/js/2.043f57b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d6f2705994ad452be3",
+    "url": "/dikshajiwane.github.io/contactmanager/static/js/main.ab92090a.chunk.js"
+  },
+  {
+    "revision": "d18f03e848f0e06051a2",
+    "url": "/dikshajiwane.github.io/contactmanager/static/js/runtime-main.d2875f23.js"
+  }
+]);
